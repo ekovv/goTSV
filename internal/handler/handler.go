fd05a5ed836589cc5fd0dbf5bfe8d81ed9a83abd
ekovv <denisovdima2004@yandex.ru>
2023-11-22 18:52:00 +0300

    :sparkles: migrations ✅

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,32 @@
-package main
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"goTSV/internal/service"
+)
+
+type Handler struct {
+	service service.Service
+	engine  *gin.Engine
+}
+
+func NewHandler(service service.Service) *Handler {
+	router := gin.Default()
+	h := &Handler{
+		service: service,
+		engine:  router,
+	}
+	Route(router, h)
+	return h
+}
+
+func (s *Handler) Start() {
+	err := s.engine.Run("localhost:8080")
+	if err != nil {
+		return
+	}
+}
+
+func (s *Handler) GetAll(c *gin.Context) {
+
+}
--- internal/shema/shema.go
+++ internal/shema/shema.go
@@ -4,7 +4,7 @@ type Tsv struct {
 	Number       string `tsv:"n"`
 	MQTT         string `tsv:"mqtt"`
 	InventoryID  string `tsv:"invid"`
-	UnitGUID     string `tsv:"unit_guid" gorm:"not null"`
+	UnitGUID     string `tsv:"unit_guid"`
 	MessageID    string `tsv:"msg_id"`
 	MessageText  string `tsv:"text"`
 	Context      string `tsv:"context"`
@@ -23,26 +23,7 @@ type Files struct {
 	Err  error
 }
 
-//CREATE TABLE checkedFiles (
-//name VARCHAR(255) PRIMARY KEY,
-//error VARCHAR(255)
-//);
-//
-//CREATE TABLE Occurrence (
-//ID           SERIAL PRIMARY KEY,
-//Number       int,
-//MQTT         VARCHAR(255),
-//InventoryID  VARCHAR(255),
-//UnitGUID     VARCHAR(255),
-//MessageID    VARCHAR(255),
-//MessageText  TEXT,
-//Context      VARCHAR(255),
-//MessageClass VARCHAR(255),
-//Level        INTEGER,
-//Area         VARCHAR(255),
-//Address      VARCHAR(255),
-//Block        BOOLEAN,
-//Type         VARCHAR(255),
-//Bit          INTEGER,
-//InvertBit    INTEGER
-//);
+type Request struct {
+	UnitGUID string `json:"unit_guid"`
+	Limit
+}
